86c6b2d9995a35ac65b3a7d8b6accd7540d27a7d
ThomasJunger <thomasjunger@gmx.at>
2019-04-07 09:32:33 +0200

    update script, added Multiplikative Operatoren, DebugLog

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Prog : MonoBehaviour {
+public class ProgBasics : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log("erste Tests mit Operatoren");
+		Debug.Log("Multiplikative Operatoren 3*4= : * " + (3*4));
+		Debug.Log("Multiplikative Operatoren 16/4= : / " + (16/4));
+		Debug.Log("Multiplikative Operatoren 10%3= : % " + (10%3));
+		
 		
 	}
 	
