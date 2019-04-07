25a4602aa9c1d451098f96a89f4cb6afbd79fae4
ThomasJunger <thomasjunger@gmx.at>
2019-04-07 09:45:59 +0200

    update script, added Relationale Operatoren, updated Multiplikative Operatoren, DebugLog

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -8,15 +8,19 @@ public class ProgBasics : MonoBehaviour {
 	void Start () {
 		Debug.Log("erste Tests mit Operatoren");
 
-		Debug.Log("Multiplikative Operatoren 3*4= : * " + (3*4));
-		Debug.Log("Multiplikative Operatoren 16/4= : / " + (16/4));
-		Debug.Log("Multiplikative Operatoren 10%3= : % " + (10%3));
+		Debug.Log("Multiplikative Operatoren: 3*4= " + (3*4));
+		Debug.Log("Multiplikative Operatoren: 16/4= " + (16/4));
+		Debug.Log("Multiplikative Operatoren: 10%3= " + (10%3));
 
 		Debug.Log("Additive Operatoren: 1+5=  " + (1+5));
 		Debug.Log("Additive Operatoren: 9-6=  " + (9-6));
 
-
+		Debug.Log("Relationale Operatoren: 9>6=  " + (9>6));
+		Debug.Log("Relationale Operatoren: 9<6=  " + (9<6));
+		Debug.Log("Relationale Operatoren: 5>=5=  " + (5>=5));
+		Debug.Log("Relationale Operatoren: 6<=6=  " + (6>=6));
 		
+
 		
 	}
 	
