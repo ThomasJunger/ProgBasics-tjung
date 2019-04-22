7f04eb56d76b657b3de227f186c62f72b2730644
ThomasJunger <thomasjunger@gmx.at>
2019-04-22 22:10:42 +0200

    experiments with variables and different type-assignments

--- Assets/MyGame/Scripts/ProgBasics.cs
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class ProgBasics : MonoBehaviour 
 {
 	public int test1;
-	public float test2;
+	public bool test2;
 	public string test3;
+	public float test4;
+	public double test5;
+	public decimal test6;
+	public sbyte test7;
+	public short test8;
+	public long test9;
 
 	// Use this for initialization
 	void Start () 
@@ -21,13 +27,15 @@ public class ProgBasics : MonoBehaviour
 		Combine();
 		Debug.Log(MyText("tjung"));
 		string drama = MyText("tjung");
-		Debug.Log("neu " + drama); 
-
-	}
-
-	public string MyText(string myName)
-	{
-		return "Drama" + " " + myName;
+		Debug.Log("neu " + drama);
+		//Variablen Experiment
+		FloatingpointTesting();
+		DecimalTesting();
+		IntegralTesting();
+		BoolTesting();
+		StringTesting();
+		//Return Type Test
+		Debug.Log(ReturnGrade(5));
 	}
 
 	public void Operators()
@@ -53,6 +61,62 @@ public class ProgBasics : MonoBehaviour
 		Debug.Log("Logische Operatoren: &=  " + (false&&true));
 		Debug.Log("Logische Operatoren: |=  " + (false|true));
 		Debug.Log("Logische Operatoren: ||=  " + (true||false));
+	}
+
+	//experiments with variables and different type-assignments
+	public void FloatingpointTesting()
+	{
+		test4 = 4.623f;
+		Debug.Log("floatvalue is " + test4);
+
+		test5 = 4.43213;
+		Debug.Log("doublevalue is " + test5);
+	}
+
+	public void DecimalTesting()
+	{
+		test6 = 4.2332322m;
+		Debug.Log("decimalvalue is " + test6);
+	}
+
+	public void IntegralTesting()
+	{
+		test1 = 2147483647;
+		Debug.Log("the maximum range of int is " + test1);
+		
+		test7 = 127;
+		Debug.Log("the maximum range of sbyte is " + test7);
+
+		test8 = 32767;
+		Debug.Log("the maximum range of short is " + test8);
+
+		test9 = 9223372036854775807;
+		Debug.Log("the maximum range of long is " + test9);
+	}
+
+	public void BoolTesting()
+	{
+		test2 = false;
+		Debug.Log("boolvalue is " + test2);
+	}
+
+	public void StringTesting()
+	{
+		test3 = "world";
+		Debug.Log("hello " + test3);
+	}
+
+	//experiments with return type and parameter
+	public string ReturnGrade(int MyGrade)
+	{
+		return "My grade is" + " " + MyGrade;
+	}
+
+
+	//experiments we did in class:
+	public string MyText(string myName)
+	{
+		return "Drama" + " " + myName;
 	}
 
 	public void XY()
