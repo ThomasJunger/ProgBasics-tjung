51b7b21db680db4a41a3dfb5a72b6a6d0b16ff4b
ThomasJunger <thomasjunger@gmx.at>
2019-05-07 10:41:21 +0200

    added ChangeText script and tried Randomize script

---
+++ Assets/MyGame/Scripts/ChangeText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//adding namespaces
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeText : MonoBehaviour {
+
+	public Text myText;
+	public TextMeshProUGUI myTMPText;
+
+	private int countPresses = 0;
+
+	private System.Random rnd = new System.Random();
+	// Use this for initialization
+	void Start () {
+		myText.text = "Halloooo";
+		myTMPText.text = "nochmal halloo";
+		myText.fontSize = 40;
+		Debug.Log(rnd.Next(0, 5));
+	}
+
+	public void addPress()
+	{
+		countPresses++;
+		//the same as countPresses = countPresses +1;
+		//and countPresses += 1;
+		Debug.Log("counts: " + countPresses);
+		myText.text = countPresses.ToString();
+		myTMPText.text = countPresses.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour 
+{
+	public void LoadIntroScene()
+	{
+		SceneManager.LoadScene("IntroScene");
+	}
+
+	public void LoadMainScene()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+
+	public void LoadEndScene()
+	{
+		SceneManager.LoadScene("EndScene");
+	}
+}
