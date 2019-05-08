432a9b4b2e5048b17a6005d787992da144c21786
ThomasJunger <thomasjunger@gmx.at>
2019-05-08 15:19:25 +0200

    tried Randomizer, generated random x-Axis position for cube

--- Assets/MyGame/Scripts/ChangeText.cs
+++ Assets/MyGame/Scripts/ChangeText.cs
@@ -9,16 +9,20 @@ public class ChangeText : MonoBehaviour {
 
 	public Text myText;
 	public TextMeshProUGUI myTMPText;
+	public TestingRandomizer myScript;
 
 	private int countPresses = 0;
 
 	private System.Random rnd = new System.Random();
+	private System.Random seedRnd = new System.Random(123);
+	
 	// Use this for initialization
 	void Start () {
 		myText.text = "Halloooo";
 		myTMPText.text = "nochmal halloo";
 		myText.fontSize = 40;
-		Debug.Log(rnd.Next(0, 5));
+		Debug.Log("++++++++++++++++++++++++++++" + rnd.Next(0, 5));
+		Debug.Log("++++++++++++++++++++++++++++" + UnityEngine.Random.Range(0,1));
 	}
 
 	public void addPress()
@@ -29,6 +33,7 @@ public class ChangeText : MonoBehaviour {
 		Debug.Log("counts: " + countPresses);
 		myText.text = countPresses.ToString();
 		myTMPText.text = countPresses.ToString();
+		Debug.Log("++++++++++++++++++++++++++++ seed " + seedRnd.Next(0, 5));
 		
 	}
 	
---
+++ Assets/MyGame/Scripts/TestingRandomizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingRandomizer : MonoBehaviour {
+
+	private float rndVal;
+	private float rndVal2;
+	public float minValX = -6f;
+	public float maxValX = 6f;
+	private System.Random rndSys = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+		rndVal = rndSys.Next(0, 10);
+		rndVal2 = UnityEngine.Random.Range(4, 100);
+		Debug.Log("System Random " + rndVal + " Unity Random " + rndVal2);
+		
+
+	}
+
+	public void ChangeRandomCube()
+	{
+		gameObject.transform.position = new Vector3(UnityEngine.Random.Range(minValX,maxValX), 0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
